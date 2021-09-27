3728af94219e047dd68a0f8d60b36f9337bd3d83
Jawwad Hassan <jh@embrace-it.com>
2021-09-27 17:55:01 +0500

    fix: register DioClient in di

--- modules/identity/example/lib/di/components/injection.dart
+++ modules/identity/example/lib/di/components/injection.dart
@@ -1,3 +1,4 @@
+import 'package:core/clients/remote_api_client/dio/dio_client.dart';
 import 'package:core/core.dart';
 import 'package:identity/identity.dart';
 import 'package:injectable/injectable.dart';
@@ -9,17 +10,22 @@ Future<void> configureInjection() async {
   final gh = GetItHelper(getIt);
   final localModule = _LocalModule();
   final networkModule = _NetworkModule();
+
+  gh.singleton<DioClient>(networkModule.dioClient);
   await gh.singletonAsync<ILocalDbClient>(
       () => localModule.provideLocalDbClient(),
       preResolve: true);
   gh.singleton<IRemoteApiClient<BaseResponse>>(networkModule.remoteApiClient);
 
   configureIdentityInjection();
+
   gh.lazySingleton<AuthInterceptor>(() => networkModule.authInterceptor);
   gh.lazySingleton<RetryInterceptor>(() => networkModule.retryInterceptor);
 
   networkModule.addInterceptors(
-      getIt<AuthInterceptor>(), getIt<RetryInterceptor>());
+    getIt<AuthInterceptor>(),
+    getIt<RetryInterceptor>(),
+  );
 }
 
 class _LocalModule extends LocalModule {}
