1e665cfc46efd02aa8f5028deef134648b44278b
Jawwad Hassan <jh@embrace-it.com>
2021-10-14 12:42:16 +0500

    refactor: cleanup code

--- modules/core/lib/models/data_state/data_state.dart
+++ modules/core/lib/models/data_state/data_state.dart
@@ -5,7 +5,8 @@ part 'data_state.freezed.dart';
 @freezed
 class DataState<T> with _$DataState {
   const factory DataState.success(T data) = Data;
-  const factory DataState.loading({@Default(-1) int progress, T? data}) = Loading;
+  const factory DataState.loading({@Default(-1) int progress, T? data}) =
+      Loading;
   const factory DataState.error(Exception error, {T? data}) = Error;
   const factory DataState.idleOrNoData() = IdleOrNoData;
 }
