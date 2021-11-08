4b2498e4aed1caa2d318324cc1338a809c384e1c
Jawwad Hassan <jh@embrace-it.com>
2021-11-08 17:35:15 +0500

    feat: add data_super_state.dart transformation extension

--- modules/core/lib/models/data_state/data_super_state.dart
+++ modules/core/lib/models/data_state/data_super_state.dart
@@ -5,8 +5,31 @@ part 'data_super_state.freezed.dart';
 @freezed
 class DataSuperState<T> with _$DataSuperState {
   const factory DataSuperState.success(T data) = SuperSuccess;
+
   const factory DataSuperState.loading({@Default(-1) int progress, T? data}) =
       SuperLoading;
+
   const factory DataSuperState.error(Exception error, {T? data}) = SuperError;
+
   const factory DataSuperState.idleOrNoData() = SuperIdleOrNoData;
+}
+
+extension DataSuperStateTransformations<T> on DataSuperState<T> {
+  DataSuperState<T> transformOnlyWhen<TResult extends Object?>({
+    DataSuperState<T> Function(T data)? success,
+    DataSuperState<T> Function(int progress, T? data)? loading,
+    DataSuperState<T> Function(Exception error, T? data)? error,
+    DataSuperState<T> Function()? idleOrNoData,
+  }) =>
+      this.map(
+        success: (value) =>
+            success == null ? value as DataSuperState<T> : success(value as T),
+        loading: (value) => loading == null
+            ? value as DataSuperState<T>
+            : loading(value.progress, value.data as T),
+        error: (value) => error == null
+            ? value as DataSuperState<T>
+            : error(value.error, value.data as T),
+        idleOrNoData: (value) => value as DataSuperState<T>,
+      );
 }
