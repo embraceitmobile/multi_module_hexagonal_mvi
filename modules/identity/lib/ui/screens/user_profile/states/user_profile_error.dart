4b06296ee6eea02d7071e49c93534c0b02878df3
Jawwad Hassan <jh@embrace-it.com>
2021-12-23 21:17:38 +0500

    feat: complete user_profile_error.dart screen

--- modules/identity/lib/ui/screens/user_profile/states/user_profile_error.dart
+++ modules/identity/lib/ui/screens/user_profile/states/user_profile_error.dart
@@ -1,4 +1,6 @@
+import 'package:core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:identity/identity.dart';
 
 class UserProfileError extends StatelessWidget {
   final Exception error;
@@ -7,7 +9,80 @@ class UserProfileError extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/img_connection_failed.png",
+                      package: "identity",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).maybePop(),
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black87,
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Oops!!",
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            SizedBox(height: 16),
+            Text(
+              "Something wrong with your connection, please try again.",
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await getIt<UserReader>().activeUser;
+                } catch (ex) {
+                  print(ex);
+                }
+              },
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 48.0, vertical: 12),
+                child: Text(
+                  "Retry",
+                  style: Theme.of(context)
+                      .textTheme
+                      .button
+                      ?.copyWith(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0),
+                    side: BorderSide(color: Theme.of(context).primaryColor),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
