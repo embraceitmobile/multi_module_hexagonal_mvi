1a0ddc7b7180ae290b85af631dd4eab2540d2956
Jawwad Hassan <jh@embrace-it.com>
2021-12-25 21:44:27 +0500

    refactor: use default RootPage for identity example

--- modules/identity/example/lib/di/route_module.dart
+++ modules/identity/example/lib/di/route_module.dart
@@ -1,7 +1,5 @@
 import 'package:core/core.dart';
-import 'package:flutter/material.dart';
 import 'package:identity/identity.dart';
-import 'package:identity/ui/shared_widgets/centered_progress_indicator.dart';
 import 'package:injectable/injectable.dart';
 
 @module
@@ -16,16 +14,5 @@ abstract class RouteModule {
 
   @singleton
   RouteHandler provideRouteHandler(IdentityRouter identityRouter) =>
-      RouteHandler([identityRouter], root: RootScreen());
-}
-
-class RootScreen extends StatelessWidget {
-  const RootScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: CenteredProgressIndicator(),
-    );
-  }
+      RouteHandler([identityRouter]);
 }
