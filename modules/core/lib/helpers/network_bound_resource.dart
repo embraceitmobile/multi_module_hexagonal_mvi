2f49ecaa06db020e543f76a000207eb1afe61a9b
Jawwad Hassan <jh@embrace-it.com>
2022-01-19 20:40:57 +0500

    feat: implement dataListener stream in network_bound_resource.dart

--- modules/core/lib/helpers/network_bound_resource.dart
+++ modules/core/lib/helpers/network_bound_resource.dart
@@ -1,29 +1,63 @@
-import 'package:flutter/foundation.dart';
+import 'dart:async';
 
-typedef RemoteCallBuilder<T> = Future<T?> Function(Future networkCall);
+import 'package:async/async.dart';
+import 'package:core/core.dart';
+import 'package:flutter/foundation.dart';
 
 class NetworkBoundResource<T> {
   static const TAG = "NetworkBoundResource";
 
-  ///Called with the data in the database to decide whether it should be
-  ///fetched from the network
+  /// Called with the data in the database to decide whether it should be
+  /// fetched from the network
   final AsyncValueGetter<bool> shouldFetch;
 
-  ///Called to get the cached data [T] from the database
+  /// Called to get the cached data [T] from the database
   final AsyncValueGetter<T?> onFetchCachedData;
 
-  ///Called to create the API call
+  /// Called to create the API call
   final AsyncValueGetter<T> onFetchFromRemoteDatasource;
 
-  ///Called to save the result [T] of the API response into the database
+  /// Called to save the result [T] of the API response into the database
   final AsyncValueSetter<T> onSaveResultToCache;
 
-  const NetworkBoundResource({
+  /// A [Stream] to observe the changes in the local data
+  /// To convert a [Stream] of [T] to a [Stream] of [DataState] of [T], you can
+  /// use the simple extension method [StreamTransformer.toDataStateStream] or
+  /// write you own custom transformer.
+  final Stream<DataState<T>>? localDatasourceListener;
+
+  /// Listen to this [Stream] to observe the changes in the [NetworkBoundResource]
+  /// data [T]. It will emit new events whenever there is a change in the [DataState]
+  /// of the remoteDataSource stream [_remoteStreamController] or the [localDatasourceListener]
+  /// if it is provided.
+  late final Stream<DataState<T>> dataListener;
+
+  /// Controls the events for the [onFetchFromRemoteDatasource] and [fetchOnceFromRemoteDatasource].
+  late final StreamController<DataState<T>> _remoteStreamController;
+
+  NetworkBoundResource({
     required this.shouldFetch,
     required this.onFetchCachedData,
     required this.onFetchFromRemoteDatasource,
     required this.onSaveResultToCache,
-  });
+    this.localDatasourceListener,
+  }) {
+    _remoteStreamController = StreamController<DataState<T>>.broadcast(
+      onListen: () async {
+        try {
+          await fetch();
+        } catch (e) {
+          print(
+              "[$TAG] Received the following error when trying to fetch for the first time. \n$e");
+        }
+      },
+    );
+
+    dataListener = StreamGroup.mergeBroadcast([
+      _remoteStreamController.stream,
+      if (localDatasourceListener != null) localDatasourceListener!,
+    ]);
+  }
 
   Future<T?> fetch({bool alwaysFetchFreshData = false}) async {
     if (!alwaysFetchFreshData) {
@@ -37,32 +71,49 @@ class NetworkBoundResource<T> {
     }
 
     if (!(await shouldFetch())) {
-      print("[$TAG] shouldFetch == false, not fetching fresh data");
+      print("[$TAG] shouldFetch = false, not fetching fresh data");
       return null;
     }
 
     try {
+      _remoteStreamController.sink.add(DataState.loading());
       final freshData = await onFetchFromRemoteDatasource();
       if (freshData != null) {
         await onSaveResultToCache(freshData);
         return freshData;
       }
-    } catch (e) {
+    } on Exception catch (e) {
+      _remoteStreamController.sink.add(DataState.error(e));
       rethrow;
     }
   }
 
-  ///Call if you have alternate API or secondary params to fetch the data
+  /// Call if you have alternate API or secondary params to fetch the data once
   Future<T?> fetchOnceFromRemoteDatasource(
       AsyncValueGetter<T?> networkCall) async {
     try {
+      _remoteStreamController.sink.add(DataState.loading());
       final data = await networkCall();
       if (data != null) {
         await onSaveResultToCache(data);
         return data;
       }
-    } catch (e) {
+    } on Exception catch (e) {
+      _remoteStreamController.sink.add(DataState.error(e));
       rethrow;
     }
   }
+
+  Future<void> dispose() async {
+    try {
+      await _remoteStreamController.close();
+    } catch (_) {
+      rethrow;
+    }
+  }
+}
+
+extension StreamTransformer<T> on Stream<T?> {
+  Stream<DataState<T>> get toDataStateStream => this.asyncMap((event) =>
+      event == null ? DataState.nothing() : DataState.success(event));
 }
