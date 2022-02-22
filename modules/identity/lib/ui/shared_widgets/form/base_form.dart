6bf93c0b082d54078b77f9aef37f1b440e8f48b9
jawwad_hassan <jh@embrace-it.com>
2022-02-22 12:40:41 +0500

    refactor: update validator signature to accept param of type T instead of String

--- modules/identity/lib/ui/shared_widgets/form/base_form.dart
+++ modules/identity/lib/ui/shared_widgets/form/base_form.dart
@@ -1,84 +1,112 @@
 import 'package:flutter/widgets.dart';
 
-typedef InputValidator = bool Function(String? value);
+typedef InputValidator<T> = bool Function(T? value);
 
 mixin FormElement {}
 
 class FormSeparator with FormElement {}
 
-class FormInput<T extends Object> with FormElement {
+abstract class FormInput<T extends Object> with FormElement {
   final String caption;
-  final TextEditingController controller;
+  final TextEditingController _controller;
   final String? hint;
-  final T? data;
   final TextInputType? inputType;
-  late final ValueGetter<bool>? validator;
+
+  ValueGetter<bool>? get validator;
+
+  T? get data;
 
   FormInput({
     required this.caption,
     this.hint,
-    this.data,
     this.inputType,
-    InputValidator? validator,
-  }) : controller = TextEditingController(text: data.toString()) {
-    if (validator != null) {
-      this.validator = () => validator.call(controller.text);
-    }
-  }
+    T? data,
+  }) : _controller = TextEditingController(text: data.toString());
 
-  void dispose() => controller.dispose();
+  void dispose() => _controller.dispose();
 }
 
-class TextFormInput extends FormInput<String> {
+abstract class StringFormInput extends FormInput<String> {
+  late final InputValidator<String>? _validator;
+
+  StringFormInput(
+      {required String caption,
+      String? hint,
+      String? data,
+      InputValidator<String>? validator,
+      TextInputType? inputType})
+      : _validator = validator,
+        super(caption: caption, hint: hint, data: data, inputType: inputType);
+
+  @override
+  String? get data => _controller.text;
+
+  @override
+  ValueGetter<bool>? get validator {
+    if (_validator == null) return null;
+    return () => _validator!.call(data);
+  }
+}
+
+class NumberFormInput extends FormInput<num> {
+  late final InputValidator<num>? _validator;
+
+  NumberFormInput(
+      {required String caption,
+      String? hint,
+      num? data,
+      InputValidator<num>? validator,
+      bool signed = false,
+      bool decimal = false})
+      : _validator = validator,
+        super(
+            caption: caption,
+            hint: hint,
+            data: data,
+            inputType: signed && decimal
+                ? TextInputType.numberWithOptions(
+                    signed: signed, decimal: decimal)
+                : TextInputType.number);
+
+  @override
+  num? get data => num.tryParse(_controller.text);
+
+  @override
+  ValueGetter<bool>? get validator {
+    if (_validator == null) return null;
+    return () => _validator!.call(data);
+  }
+}
+
+class TextFormInput extends StringFormInput {
   TextFormInput(
       {required String caption,
       String? hint,
       String? data,
-      InputValidator? validator,
+      InputValidator<String>? validator,
       bool isSingleLine = false})
       : super(
             caption: caption,
             hint: hint,
             data: data,
-            validator: validator,
             inputType:
                 isSingleLine ? TextInputType.text : TextInputType.multiline);
 }
 
-class NumberFormInput extends FormInput<num> {
-  NumberFormInput(
-      {required String caption,
-      String? hint,
-      num? data,
-      InputValidator? validator,
-      bool signed = false,
-      bool decimal = false})
-      : super(
-            caption: caption,
-            hint: hint,
-            data: data,
-            validator: validator,
-            inputType: signed && decimal
-                ? TextInputType.numberWithOptions(
-                    signed: signed, decimal: decimal)
-                : TextInputType.number);
-}
-
-class PasswordFormInput extends FormInput<String> {
+class PasswordFormInput extends StringFormInput {
   PasswordFormInput(
       {required String caption,
       String? hint,
       String? data,
-      InputValidator? validator})
+      InputValidator<String>? validator})
       : super(
             caption: caption,
             hint: hint,
             data: data,
-            validator: validator,
             inputType: TextInputType.visiblePassword);
 }
 
-class PhoneFormInput extends FormInput<String> {
+class PhoneFormInput extends StringFormInput {
   PhoneFormInput(
       {required String caption,
       String? hint,
@@ -92,7 +120,7 @@ class PhoneFormInput extends FormInput<String> {
             inputType: TextInputType.phone);
 }
 
-class EmailFormInput extends FormInput<String> {
+class EmailFormInput extends StringFormInput {
   EmailFormInput(
       {required String caption,
       String? hint,
@@ -106,7 +134,7 @@ class EmailFormInput extends FormInput<String> {
             inputType: TextInputType.emailAddress);
 }
 
-class UrlFormInput extends FormInput<String> {
+class UrlFormInput extends StringFormInput {
   UrlFormInput(
       {required String caption,
       String? hint,
@@ -120,16 +148,26 @@ class UrlFormInput extends FormInput<String> {
             inputType: TextInputType.url);
 }
 
-class DateTimeFormInput extends FormInput<String> {
+class DateTimeFormInput extends FormInput<DateTime> {
+  late final InputValidator<DateTime>? _validator;
+
   DateTimeFormInput(
       {required String caption,
       String? hint,
-      String? data,
-      InputValidator? validator})
+      DateTime? data,
+      InputValidator<DateTime>? validator})
       : super(
             caption: caption,
             hint: hint,
             data: data,
-            validator: validator,
             inputType: TextInputType.datetime);
+
+  @override
+  DateTime? get data => DateTime.tryParse(_controller.text);
+
+  @override
+  ValueGetter<bool>? get validator {
+    if (_validator == null) return null;
+    return () => _validator!.call(data);
+  }
 }
