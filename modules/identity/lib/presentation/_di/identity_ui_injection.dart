84d14b3c7a5d4d2a9b415b4f2906b6ac913c85b0
jawwad_hassan <jh@embrace-it.com>
2022-03-16 10:52:07 +0500

    chores: cleanup code

--- modules/identity/lib/presentation/_di/identity_ui_injection.dart
+++ modules/identity/lib/presentation/_di/identity_ui_injection.dart
@@ -3,7 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:identity/presentation/screens/login/stores/login_form_store.dart';
 import 'package:injectable/injectable.dart';
 
-GetIt configureIdentityUiInjection() {
+GetIt configureIdentityPresentationInjection() {
   final gh = GetItHelper(getIt);
 
   gh.lazySingleton<LoginFormStore>(() => LoginFormStore());
